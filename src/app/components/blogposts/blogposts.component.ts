aa59fa32540680e3ae01f78bede12244dc48f1bd
JanssensStijn <114300241+JanssensStijn@users.noreply.github.com>
2024-11-07 16:02:48 +0100

    blogservice

--- src/app/components/blogposts/blogposts.component.ts
+++ src/app/components/blogposts/blogposts.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BlogPost } from '../../interfaces/blog-post';
+import { BlogService } from '../../services/blog.service';
 
 @Component({
   selector: 'app-blogposts',
@@ -9,64 +10,13 @@ import { BlogPost } from '../../interfaces/blog-post';
   styleUrl: './blogposts.component.css'
 })
 export class BlogpostsComponent {
-  blogposts: BlogPost[] = [
-    {
-      title: "The Birth of Stars",
-      text: "Discover the fascinating process of star formation in nebulas.",
-      imageUrl: "https://spaceholder.cc/i/800x600",
-      postDate: "July 12, 2023",
-      author: "Dr. Jane Smith",
-      readMoreLink: "#",
-      liked: false
-    },
-    {
-      title: "Journey to Mars",
-      text: "Explore the challenges and goals of human missions to the Red Planet.",
-      imageUrl: "https://spaceholder.cc/i/400x300",
-      postDate: "March 8, 2024",
-      author: "Prof. Mark Johnson",
-      readMoreLink: "#",
-      liked: false
-    },
-    {
-      title: "Black Holes: Mysteries Unveiled",
-      text: "Dive into the enigmatic world of black holes and their powerful forces.",
-      imageUrl: "https://spaceholder.cc/i/400x301",
-      postDate: "November 23, 2022",
-      author: "Dr. Emily Davis",
-      readMoreLink: "#",
-      liked: false
-    },
-    {
-      title: "The Expanding Universe",
-      text: "Learn about the ongoing expansion of the universe and its implications.",
-      imageUrl: "https://spaceholder.cc/i/400x303",
-      postDate: "January 15, 2024",
-      author: "Dr. Robert Brown",
-      readMoreLink: "#",
-      liked: false
-    },
-    {
-      title: "The Milky Way: Our Galactic Home",
-      text: "Take a closer look at our galaxy and its stunning structure.",
-      imageUrl: "https://spaceholder.cc/i/400x305",
-      postDate: "June 30, 2023",
-      author: "Dr. Linda Wilson",
-      readMoreLink: "#",
-      liked: false
-    },
-    {
-      title: "Exoplanets: New Worlds",
-      text: "Discover planets beyond our solar system and the search for alien life.",
-      imageUrl: "https://spaceholder.cc/i/400x307",
-      postDate: "September 19, 2023",
-      author: "Prof. David M.",
-      readMoreLink: "#",
-      liked: false
-    }
-  ];
+  blogposts: BlogPost[] ;
+
+  constructor(private blogService: BlogService) {
+    this.blogposts = this.blogService.getAllBlogPosts();
+  }
 
   toggleLikePost(post: BlogPost) {
-    post.liked = !post.liked;
+    this.blogService.toggleLikePost(post);
   }
 }
