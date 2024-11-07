69fe8f4fcd656c29e560f4bcd5b7de69c54c3c62
JanssensStijn <114300241+JanssensStijn@users.noreply.github.com>
2024-11-07 10:30:22 +0100

    binding home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  planet = {
+    name: "EARTH",
+    nickname: "The Blue Planet",
+    description: "Earth is the third planet from the Sun and the only astronomical object known to harbor and support life. About 29.2% of Earth's surface is land consisting of continents and islands.",
+    imgUrl: "img/earth.png",
+    wikipediaUrl: "https://en.wikipedia.org/wiki/Earth"
+};
 }
