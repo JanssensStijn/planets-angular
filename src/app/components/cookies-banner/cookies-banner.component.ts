3312138711135ad4df3230d5a6fc1763b07e09fb
JanssensStijn <114300241+JanssensStijn@users.noreply.github.com>
2024-11-07 11:55:13 +0100

    *ngIf to @If

--- src/app/components/cookies-banner/cookies-banner.component.ts
+++ src/app/components/cookies-banner/cookies-banner.component.ts
@@ -1,10 +1,10 @@
-import { CommonModule } from '@angular/common';
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cookies-banner',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './cookies-banner.component.html',
   styleUrl: './cookies-banner.component.css'
 })
