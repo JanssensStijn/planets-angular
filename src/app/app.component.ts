ee6198f537bd9e8d05cd3c1ab4565b6de4805d9b
JanssensStijn <114300241+JanssensStijn@users.noreply.github.com>
2024-11-07 11:43:42 +0100

    cookiebanner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CookiesBannerComponent } from './components/cookies-banner/cookies-banner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, 
+  imports: [
     NavbarComponent, 
+    RouterOutlet,
+    CookiesBannerComponent,
     FooterComponent,
   ],
   templateUrl: './app.component.html',
---
+++ src/app/components/cookies-banner/cookies-banner.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cookies-banner',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cookies-banner.component.html',
+  styleUrl: './cookies-banner.component.css'
+})
+export class CookiesBannerComponent {
+  setCookie(name: string, value: string, days: number): void {
+      const date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      const expires = "expires=" + date.toUTCString();
+      document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  }
+  
+  getCookie(name: string): string | null {
+      const nameEQ = name + "=";
+      const ca = document.cookie.split(';');
+      for (let i = 0; i < ca.length; i++) {
+          let c = ca[i];
+          while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+          if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+      }
+      return null;
+  }
+  
+  acceptCookies(): void {
+      this.setCookie('cookiesAccepted', 'true', 365);
+  }
+  
+  areCookiesAccepted(): boolean {
+      return this.getCookie('cookiesAccepted') === 'true';
+  }
+}
